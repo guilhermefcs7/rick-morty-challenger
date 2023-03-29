81075e5ef69ffca2aac2462ef2a5aa75d51987d6
Guilherme Falcão <guifalcao2010@hotmail.com>
2023-03-29 16:53:53 -0300

    project: includes more colors in colors object

--- constants/colors.ts
+++ constants/colors.ts
@@ -1,6 +1,10 @@
 export const colors = {
   white: "#fff",
   black: "#000",
+  gray: "#333",
   gray900: "#121214",
   gray100: "#e1e1e6",
+  chiffonBlue: "#99dafb",
+  irisBlue: "#00bcd4",
+  turquoiseBlue: "#61dafb",
 };
